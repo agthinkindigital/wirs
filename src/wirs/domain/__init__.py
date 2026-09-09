ca5d996d3382824d5fe30b5f00ca56a4f37acf93
Leonardo Nascimento <agenciathinkindigital@gmail.com>
2026-09-08 22:02:27 -0300

    feat: Artifact model com kinds, metadata, from_stat e round-trip

--- src/wirs/domain/__init__.py
+++ src/wirs/domain/__init__.py
@@ -3,11 +3,15 @@
 Proibido importar wordpress, yara, wordfence, rich, mysql, typer a partir daqui.
 """
 
+from wirs.domain.artifact import Artifact, ArtifactKind, ArtifactMetadata
 from wirs.domain.errors import SecurityBoundaryError, TargetError, WirsError
 from wirs.domain.safepath import SafePath
 from wirs.domain.target import LocalDirectoryTarget, Target, TargetKind
 
 __all__ = [
+    "Artifact",
+    "ArtifactKind",
+    "ArtifactMetadata",
     "LocalDirectoryTarget",
     "SafePath",
     "SecurityBoundaryError",
